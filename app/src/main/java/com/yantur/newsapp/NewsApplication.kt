ebb5ffc874f9615b0cfc991786bff7086454caff
arthurakhnoyan <arthur.akhnoyan@picsart.com>
2020-02-18 14:42:18 +0400

    art

--- app/src/main/java/com/yantur/newsapp/NewsApplication.kt
+++ app/src/main/java/com/yantur/newsapp/NewsApplication.kt
@@ -11,7 +11,7 @@ class NewsApplication : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@NewsApplication)
-            modules(appModule)
+            modules(appModule);
         }
     }
 }
